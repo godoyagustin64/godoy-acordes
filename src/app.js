f0d6549d2cefddbf104d05af51f52520628fd27b
Agustin Nahuel Godoy Gaetan <AGodoy@veng.local>
2022-09-07 12:22:16 -0300

    se agrega carpeta publica con vistas

--- src/app.js
+++ src/app.js
@@ -1,28 +1,34 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 
 app.get("/", (req,res)=>{
     res.send("Estoy en home");
-    });
+});
+
+
+//carpeta PUBLIC static
+app.use(express.static("public"));
+
 
 app.get("/detail-product", (req,res)=>{
     res.send("Estoy en detail-product");
-    });
+});
 
 app.get("/cart", (req,res)=>{
     res.send("Estoy en login");
-    });
+});
 
 app.get("/register", (req,res)=>{
     res.send("Estoy en register");
-    });
+});
 
 app.get("/login", (req,res)=>{
     res.send("Estoy en login");
-    });
+});
 
 
-    
+
 
 app.listen(3000, () => console.log(`Servidor en puerto 3000`));
